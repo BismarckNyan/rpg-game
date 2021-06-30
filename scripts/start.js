6c768f5e4076fc0735b18672ce4a4c1590faa033
Bis <negropont@gmail.com>
2021-06-30 10:15:21 +0500

    heroku4

--- scripts/start.js
+++ scripts/start.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const Hapi = require('@hapi/hapi');
 
-const port = process.PORT ||3000;
+const port = process.env.PORT || 3000;
 
 const FILES = /\.(js|js.map|woff|woff2|svg|bmp|jpg|jpeg|gif|png|ico)(\?v=\d+\.\d+\.\d+)?$/;
 
@@ -9,9 +9,8 @@ const PATH = {
     '/': 'index.html'
 }
 
-
-const init = async()=>{
-    const server =Hapi.server({
+const init = async () => {
+    const server = Hapi.server({
         port,
     });
 
@@ -19,24 +18,23 @@ const init = async()=>{
 
     server.route({
         method: 'GET',
-        path:'/{path*}',
-        handler:(request, h)=>{
-            if (FILES.test(request.path)){
-                return h.file(path.join(process.cwd(),'dist',request.path));
+        path: '/{path*}',
+        handler: (request, h) => {
+            if (FILES.test(request.path)) {
+                return h.file(path.join(process.cwd(), 'dist', request.path));
             }
-            console.log(request.path);
-            return h.file(path.join(process.cwd(),'dist',PATH[request.path]));
+
+            return h.file(path.join(process.cwd(), 'dist', PATH[request.path]));
         }
     })
 
     await server.start();
-    console.log("Server running on %s", server.info.uri);
+    console.log('Server running on %s', server.info.uri);
 }
 
-process.on ('unhandledRejection',(err)=>{
+process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
-
 });
 
 init();
