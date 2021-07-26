77aca2d7fe43b631e2ebc42f748d9811d4d09a8c
Bis <negropont@gmail.com>
2021-07-26 21:33:33 +0500

    homework-4

--- src/common/util.js
+++ src/common/util.js
@@ -1,7 +1,16 @@
 export function clamp(x, fromX, toX) {
-  if (x < fromX) x = fromX;// eslint-disable-line no-param-reassign
-  if (x > toX) x = toX;// eslint-disable-line no-param-reassign
+  if (x < fromX) x = fromX; // eslint-disable-line no-param-reassign
+  if (x > toX) x = toX; // eslint-disable-line no-param-reassign
 
   return x;
 }
-export default clamp;
+export function animateEx(dx, startTime, currentTime, speed, looped = false) {
+  const diff = currentTime - startTime;
+  let time = (speed && diff / speed) || 0;
+  if (looped) {
+    time %= 1;
+  } else if (time > 1) {
+    time = 1;
+  }
+  return { offset: dx * time, progress: time };
+}
