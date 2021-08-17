b3eaf9f10063f47ae9f4692d9438a73b461f69d4
Bis <negropont@gmail.com>
2021-08-18 00:02:04 +0500

    homework-6

--- src/index.js
+++ src/index.js
@@ -1,167 +1,56 @@
+import { io } from 'socket.io-client';
 import ClientGame from './client/ClientGame';
+import { getTime } from './common/util';
 import './index.scss';
-// import Male3Walk from './assets/Male-3-Walk.png';
-// import terrainAtlas from './assets/terrain.png';
-// import worldCfg from './configs/world.json';
-// import sprites from './configs/sprites';
-const form = document.getElementById('nameForm');
-form.name.value = '';
 
-function complete(value) {
-  ClientGame.init({ tagId: 'game', playerName: value });
-  const node = document.getElementById('start');
-  if (node.parentNode) {
-    node.parentNode.removeChild(node);
-  }
-}
-form.onsubmit = function (e) {
-  e.preventDefault();
-  const { value } = form.name;
-  if (value === '') return false;
-  complete(value);
-  return false;
-};
-
-// window.addEventListener('load', () => {
-// ClientGame.init({ tagId: 'game',playerName:form.name.value });
-// });
-
-/* const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
-const spriteW = 48;
-const spriteH = 48;
-
-const terrain = document.createElement('img');
-terrain.src=terrainAtlas;
-
-terrain.addEventListener ('load',()=>{
-  const {map}= worldCfg;
-map.forEach((cfgRow,y)=>{
-cfgRow.forEach((cfgCell,x)=>{
-const [sX,sY,sW,sH]=sprites.terrain[cfgCell[0]].frames[0];
-ctx.drawImage(terrain,sX,sY,sW,sH,x*spriteW,y*spriteH,spriteW,spriteH);
-})
-})
-});
-
-const shots = 3;
-
-let cycle = 0;
-let bottomPressed= false;
-let topPressed = false;
-let leftPressed = false;
-let rightPressed = false;
-let pY = 300;
-let pX = 275;
-let pDir = 0;
-function keyDownHandler(e) {
-  switch (e.key) {
-    case 'ArrowDown':
-    case 'Down':
-      bottomPressed = true;
-      break;
-    case 'ArrowUp':
-    case 'Up':
-      topPressed = true;
-      break;
-    case 'ArrowLeft':
-    case 'Left':
-      leftPressed = true;
-      break;
-    case 'ArrowRight':
-    case 'Right':
-      rightPressed = true;
-      break;
-    default:
-      break;
-  }
-}
-function keyUpHandler(e) {
-  switch (e.key) {
-    case 'ArrowDown':
-    case 'Down':
-      bottomPressed= false;
-      break;
-    case 'ArrowUp':
-    case 'Up':
-      topPressed = false;
-      break;
-    case 'ArrowLeft':
-    case 'Left':
-      leftPressed = false;
-      break;
-    case 'ArrowRight':
-    case 'Right':
-      rightPressed = false;
-      break;
-    default:
-      break;
-  }
-}
-
-document.addEventListener('keydown', keyDownHandler);
-document.addEventListener('keyup', keyUpHandler);
-
-const img = document.createElement('img');
-img.src = Male3Walk;
-
-img.addEventListener('load', () => {
-  setInterval(() => {
-    switch (true) {
-      case bottomPressed:
-        if (pY <= 545) {
-          pY += 10;
-        }
-        cycle = (cycle + 1) % shots;
-        pDir = 0;
-        break;
-      case topPressed:
-        if (pY >= 5) {
-          pY -= 10;
-        }
-        cycle = (cycle + 1) % shots;
-        pDir = 144;
-        break;
-      case leftPressed:
-        if (pX >= 5) {
-          pX -= 10;
-        }
-        cycle = (cycle + 1) % shots;
-        pDir = 48;
-        break;
-      case rightPressed:
-        if (pX <= 552) {
-          pX += 10;
-        }
-        cycle = (cycle + 1) % shots;
-        pDir = 96;
-        break;
-      default:
-        break;
+window.addEventListener('load', () => {
+  const socket = io('https://jsprochat.herokuapp.com');
+  const $startGame = document.getElementById('start');
+  const $nameForm = document.getElementById('nameForm');
+  const $inputName = document.getElementById('name');
+  const $chatWrap = document.querySelector('.chat-wrap');
+  const $form = document.getElementById('form');
+  const $input = document.getElementById('input');
+  const $message = document.querySelector('.message');
+  let name = '';
+  const submitName = (e) => {
+    e.preventDefault();
+    if ($inputName.value) {
+      ClientGame.init({ tagId: 'game', playerName: $inputName.value });
+      socket.emit('start', $inputName.value);
+      name = $inputName.value;
+      $chatWrap.style.display = 'block';
+      $nameForm.removeEventListener('submit', submitName);
+      $startGame.remove();
     }
+  };
+  $nameForm.addEventListener('submit', submitName);
 
-    ctx.clearRect(0, 0, 600, 600);
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, 600, 600);
-    ctx.lineWidth = 10;
-    ctx.beginPath();
-    const Base = { x: 300, y: 90 };
-    ctx.moveTo(Base.x, Base.y);
-    const radius = 240;
-    ctx.beginPath();
-    ctx.strokeStyle = '#D60606';
-    ctx.moveTo(Base.x, Base.y);
-    for (let i = 1; i <= 5; i += 1) {
-      const th = (i * 4 * Math.PI) / 5;
-      const x = Base.x + radius * Math.sin(th);
-      const y = Base.y + radius - radius * Math.cos(th);
-      ctx.lineTo(x, y);
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if ($input.value) {
+      socket.emit('chat message', $input.value);
+      $input.value = '';
     }
-    ctx.lineJoin = 'miter';
-    ctx.closePath();
-    ctx.stroke();
+  });
 
-    ctx.drawImage(img, cycle * spriteW, pDir, spriteW, spriteH, pX, pY, 48, 48);
-  }, 120);
+  socket.on('chat connection', (data) => {
+    $message.insertAdjacentHTML('beforeend', `<p><strong>${getTime(data.time)}</strong> ${data.msg}</p>`);
+  });
+  socket.on('chat online', (data) => {
+    $message.insertAdjacentHTML('beforeend', `<p>${'Current online'} ${data.online}</p>`);
+  });
+  socket.on('chat disconnect', (data) => {
+    $message.insertAdjacentHTML('beforeend', `<p><strong>${getTime(data.time)}</strong> ${data.msg}</p>`);
+  });
+  socket.on('chat message', (data) => {
+    if (data.name === name) {
+      $message.insertAdjacentHTML(
+        'beforeend',
+        `<p><strong>${getTime(data.time)}</strong> - <font color="red">${data.msg}</font></p>`,
+      );
+    } else {
+      $message.insertAdjacentHTML('beforeend', `<p><strong>${getTime(data.time)}</strong> - ${data.msg}</p>`);
+    }
+  });
 });
-*/
